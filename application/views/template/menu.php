a9b65ce2e4cf391557db0bcf50664a7ea6a9cf12
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2017-05-10 16:06:48 -0600

    MENU: Se ajusto menu

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -77,45 +77,11 @@
 		<div class="sidebar-nav navbar-collapse">
 			<ul class="nav" id="side-menu">
 				<li>
-					<a href="#">
-					
-					<?php if($this->session->photo){ ?>
-					<img src="<?php echo base_url($this->session->photo); ?>" class="img-rounded" width="26" height="26" />
-					<?php }else{?>
-					<i class="fa fa-child fa-fw"></i>
-					<?php } ?>
-					Hi <?php echo $this->session->firstname; ?>!!!<span class="fa arrow"></span></a>
-					<ul class="nav nav-second-level">
-
-						<li>
-							<a href="<?php echo base_url("employee/photo"); ?>">User Photo</a>
-						</li>
-						
-					</ul>
-					<!-- /.nav-second-level -->
+					<a href="#"><i class="fa fa-user fa-fw"></i> <?php echo $this->session->firstname; ?></a>
 				</li>
 				<li>
 					<a href="<?php echo base_url("dashboard"); ?>"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
 				</li>
-				<li>
-					<a href="#"><i class="fa fa-edit fa-fw"></i> Record Task(s)<span class="fa arrow"></span></a>
-					<ul class="nav nav-second-level">
-						<li>
-							<a href="<?php echo base_url("payroll/add_payroll"); ?>"> Payroll</a>
-						</li>
-
-						<li>
-							<a href="<?php echo base_url("safety/add_safety"); ?>"> Safety</a>
-						</li>
-
-						<li>
-							<a href="<?php echo base_url("hauling/add_hauling"); ?>"> Hauling</a>
-						</li>
-						
-					</ul>
-					<!-- /.nav-second-level -->
-				</li>
-
 			</ul>
 		</div>
 		<!-- /.sidebar-collapse -->
