b15ca77e788002dbc3388542a52136ab593d3655
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2017-05-24 09:27:41 -0600

    Validar sesion: Se coloca libreria para validar sesion del usuario

--- application/modules/admin/controllers/Admin.php
+++ application/modules/admin/controllers/Admin.php
@@ -6,6 +6,7 @@ class Admin extends MX_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model("admin_model");
+		$this->load->library("validarsesion");
     }
 	
 	/**
