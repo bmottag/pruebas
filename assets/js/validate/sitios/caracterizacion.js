bc8ac7d7094ee9e4bf447229fb74037ba82e68e4
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-02-07 10:30:13 -0700

    Sitios-caracterizacion: Validaciones

---
+++ assets/js/validate/sitios/caracterizacion.js
@@ -0,0 +1,96 @@
+$( document ).ready( function () {
+		
+	$( "#form" ).validate( {
+		rules: {
+			ubicacion:					{ required: true },
+			forma_transporte:			{ required: true },
+			transporte:					{ required: true },
+			vias:						{ required: true },
+			riesgo:						{ required: true },
+			cerramientos:				{ required: true },
+			vigilancia_privada:			{ required: true },
+			camaras:					{ required: true },
+			baterias:					{ required: true },
+			baterias_discapacitados:	{ required: true },
+			administrativa:				{ required: true },
+			fotocopiadoras:				{ required: true },
+			cafeteria_interna:			{ required: true },
+			cafeteria_externa:			{ required: true }
+			
+		},
+		errorElement: "em",
+		errorPlacement: function ( error, element ) {
+			// Add the `help-block` class to the error element
+			error.addClass( "help-block" );
+			error.insertAfter( element );
+
+		},
+		highlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".col-sm-6" ).addClass( "has-error" ).removeClass( "has-success" );
+		},
+		unhighlight: function (element, errorClass, validClass) {
+			$( element ).parents( ".col-sm-6" ).addClass( "has-success" ).removeClass( "has-error" );
+		},
+		submitHandler: function (form) {
+			return true;
+		}
+	});
+	
+	$("#btnSubmit").click(function(){		
+	
+		if ($("#form").valid() == true){
+		
+				//Activa icono guardando
+				$('#btnSubmit').attr('disabled','-1');
+				$("#div_error").css("display", "none");
+				$("#div_load").css("display", "inline");
+			
+				$.ajax({
+					type: "POST",	
+					url: base_url + "sitios/save_caracterizacion",
+					data: $("#form").serialize(),
+					dataType: "json",
+					contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+					cache: false,
+					
+					success: function(data){
+                                            
+						if( data.result == "error" )
+						{						
+								$("#div_load").css("display", "none");
+								$("#div_error").css("display", "inline");
+								$("#span_msj").html(data.mensaje);
+								$('#btnSubmit').removeAttr('disabled');
+								alert(data.mensaje);
+								return false;							
+						} 
+
+						if( data.result )//true
+						{	                                                        
+							$("#div_load").css("display", "none");
+							$('#btnSubmit').removeAttr('disabled');
+
+							var url = base_url + "sitios/caracterizacion/" + data.idRecord;
+							$(location).attr("href", url);
+						}
+						else
+						{
+							alert('Error. Reload the web page.');
+							$("#div_load").css("display", "none");
+							$("#div_error").css("display", "inline");
+							$('#btnSubmit').removeAttr('disabled');
+						}	
+					},
+					error: function(result) {
+						alert('Error. Reload the web page.');
+						$("#div_load").css("display", "none");
+						$("#div_error").css("display", "inline");
+						$('#btnSubmit').removeAttr('disabled');
+					}
+					
+		
+				});	
+		
+		}//if			
+	});
+});
